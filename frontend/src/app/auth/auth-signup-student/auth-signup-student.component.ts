116f75b3c8c1b8bcea80040494b9939ebce0945d
DusanDjordjic <dukidjordjic@gmail.com>
2021-12-28 13:45:37 +0100

    Dodane putanje na linkove, poboljšane greske i komentari

--- frontend/src/app/auth/auth-signup-student/auth-signup-student.component.ts
+++ frontend/src/app/auth/auth-signup-student/auth-signup-student.component.ts
@@ -1,11 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SubjectModel } from 'src/shared/models/subject.model';
 import { SubjectsService } from 'src/shared/providers/subjects.service';
 import { checkPasswordsValidator } from 'src/shared/validators/confirm-password.validator';
@@ -18,14 +12,23 @@ import { SignupStudentModel } from '../models/signup-student.model';
   styleUrls: ['./auth-signup-student.component.scss'],
 })
 export class AuthSignupStudentComponent implements OnInit {
+  // Selektovani niz interesovanja
   interests: SubjectModel[] = [];
+  // Niz svih predmeta
   subjects: SubjectModel[] = [];
+  // Za greške koje se mogu javiti prilikom submit-ovanja
   afterSubmitErrors: string[] = [];
+  afterSubmitErrorStatusCode: number = -1;
+  internalErrors: string[] = [];
+  // Boolean da bi smo znali kada da prikažemo greške kod selektovanja interesovanja
   interestsTouched = false;
+  // Boolean za spiner
   isLoading = false;
+  // 3 Polja na koja cemo se fokusirati u slucaju greske
   @ViewChild('usernameField') usernameField!: ElementRef;
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('phoneNumberField') phoneNumberField!: ElementRef;
+
   signupForm = new FormGroup(
     {
       firstName: new FormControl(null, [Validators.required]),
@@ -56,72 +59,124 @@ export class AuthSignupStudentComponent implements OnInit {
     private authService: AuthService
   ) {}
 
+  ngOnInit(): void {
+    // Uzimamo sve predmete sa servera
+    this.subjectService.getAllSubjects().subscribe({
+      next: (subjects) => {
+        this.subjects = subjects;
+      },
+      error: (err) => {
+        // TODO Modal da iskoci ako su se desile greske koje nisu do korisnika
+        this.internalErrors.push(
+          'Došlo je do greške. Server je u kvaru. Pokušajte opet kasnije'
+        );
+      },
+    });
+  }
+  // Getter za niz interesovanja
   get formInterests() {
     return this, this.signupForm.get('interests') as FormArray;
   }
+  // Metoda za dobijanja kontrole
   getFormControl(name: string) {
     return this.signupForm.get(name) as FormControl;
   }
-  onSubmit() {
-    this.interestsTouched = true;
 
+  // Metoda koja se poziva na submit
+  onSubmit() {
+    // Ako se nekako klinke forma dok nije validna ne treba ništa da se desi
     if (this.signupForm.invalid) return;
+
+    // Uključujemo spiner
     this.isLoading = true;
+
+    // Pravimo nog studenta od podataka iz forme
     const newStudent = new SignupStudentModel(this.signupForm.value);
 
-    this.authService.signupStudent(newStudent).subscribe(
-      (data) => {
-        console.log(data);
+    // Http zahtev za slanje studenta
+    this.authService.signupStudent(newStudent).subscribe({
+      next: (data) => {
+        // Ako se prođe kako treba gasimo spiner i postavljamo greske na prazan niz
         this.isLoading = false;
         this.afterSubmitErrors = [];
       },
-      (err) => {
-        let filedToFocus: string = '';
-        this.afterSubmitErrors = err.error.response.map(
-          (err: { field: string; message: string }) => {
-            if (filedToFocus == '') filedToFocus = err.field;
-            return err.message;
-          }
-        );
-        this.focusOnField(filedToFocus);
+      error: (err) => {
+        console.log(err);
+        // Cuvamo status code greške
+        this.afterSubmitErrorStatusCode = err.error.status;
+        // Ako je status code greske 409 (Conflict)
+        // Znači da je email, broj telefona ili username postoje
+        if (err && err.error && err.error.status == 409) {
+          // Na koje polje ćemo se fokusirti
+          let filedToFocus: string = '';
+          // Mapiramo poruke greške u niz grešaka
+          this.afterSubmitErrors = err.error.response.map(
+            (err: { field: string; message: string }) => {
+              // Postaviti focus na prvo polje na koje se naiđe
+              if (filedToFocus == '') filedToFocus = err.field;
+              return err.message;
+            }
+          );
+          // Fokusirati se na polje
+          this.focusOnField(filedToFocus);
+        } else if (err && err.error && err.error.status == 500) {
+          // Ako je status code 500 znači da je došlo do greške na serveru
+          this.afterSubmitErrors = err.error.response.map(
+            (err: { field: string; message: string }) => err.message
+          );
+        } else {
+          // Ako se desila nepredviđena greška
+          // TODO Modal da iskoci ako su se desile greske koje nisu do korisnika
+          this.internalErrors.push(
+            'Došlo je do greške. Server je u kvaru. Pokušajte opet kasnije'
+          );
+        }
+
         this.isLoading = false;
-      }
-    );
-  }
-  focusOnField(fieldName: string) {
-    if (fieldName == 'email') this.emailField.nativeElement.focus();
-    if (fieldName == 'phoneNumber') this.phoneNumberField.nativeElement.focus();
-    if (fieldName == 'username') this.usernameField.nativeElement.focus();
-  }
-  ngOnInit(): void {
-    this.subjectService.getAllSubjects().subscribe((subjects) => {
-      this.subjects = subjects;
+      },
     });
   }
-  addSubjectToArray(_id: number) {
-    this.interestsTouched = true;
-    const sub = this.subjects.find((subject) => subject._id == _id);
-    if (!sub) return;
-    if (this.interests.includes(sub)) return;
-    this.interests.push(sub);
 
+  // Fokusiranje na polje
+  focusOnField(fieldName: string) {
+    if (fieldName == 'email') {
+      // (this.emailField.nativeElement as HTMLElement).scrollIntoView();
+      this.emailField.nativeElement.focus();
+    }
+    if (fieldName == 'phoneNumber') {
+      // (this.phoneNumberField.nativeElement as HTMLElement).scrollIntoView();
+      this.phoneNumberField.nativeElement.focus();
+    }
+    if (fieldName == 'username') {
+      // (this.usernameField.nativeElement as HTMLElement).scrollIntoView();
+      this.usernameField.nativeElement.focus();
+    }
+  }
+
+  // Dodajemo predmet u niz predmeta
+  addSubjectToArray(_id: number) {
+    // Postavljamo na true zato što je pipnuto dugme
+    this.interestsTouched = true;
+    // Tražimo predmet koji ima isti id
+    const sub = this.subjects.find((subject) => subject._id == _id);
+    // Ako uposte ne postoji predmet sa tim id-jem samo se vraćamo
+    if (!sub) return;
+    // Provera da li je predmet već dodan u niz. Ako jeste samo se vratimo
+    if (this.interests.includes(sub)) return;
+
+    // Dodajemo predmet u niz za prikaz i u FormArray
+    this.interests.push(sub);
     this.formInterests.push(new FormControl(_id));
-    console.log(this.formInterests.value);
   }
 
   removeSubjectFromArray(_id: number) {
+    // Niz za prikaz filtriramo samo
     this.interests = this.interests.filter((sub) => sub._id !== _id);
+    // Za FormArray moramo da prvo pronađemo indeks pa da obrišemo sa tog indeksa
     this.formInterests.removeAt(
       this.formInterests.value.findIndex(
         (subjectId: number) => subjectId === _id
       )
     );
-    console.log(this.formInterests.value);
-  }
-  convertToFormControl(subject: AbstractControl) {
-    return subject as FormControl;
-  }
-  getSubjectName(_id: number) {
-    return this.subjects.find((subject) => subject._id == _id)?.name;
   }
 }
